2cad4ce78938c8de6940a9b24340932bfa746548
Luka <luc.bayo@gmail.com>
2025-10-09 22:04:12 +0200

    chore: improve main controller to use async handler

--- src/helpers/MainController.ts
+++ src/helpers/MainController.ts
@@ -2,6 +2,7 @@ import { getPagination } from "./query.helpers";
 import z, { ZodError, ZodObject, ZodRawShape } from "zod";
 import { Context } from "hono";
 import { DatabaseModel } from "../database/database.model";
+import { asyncHandler } from "./async.helper";
 
 export class MainController<T> extends DatabaseModel {
   limit: number = 25;
@@ -19,41 +20,19 @@ export class MainController<T> extends DatabaseModel {
     this.context = c;
   }
 
-  getAll = async () => {
-    try {
-      const limit =
-        parseInt(this.context.req.query("limit") as string) || this.limit;
-      const page =
-        parseInt(this.context.req.query("page") as string) || this.page;
-      const data = await this.findAll<T>(
-        this.table,
-        getPagination(page, limit)
-      );
-      return this.context.json(data, 200);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      return this.context.json({ error: "Internal server error" }, 500);
-    }
-  };
+  getAll = asyncHandler(async () => {
+    const limit =
+      parseInt(this.context.req.query("limit") as string) || this.limit;
+    const page =
+      parseInt(this.context.req.query("page") as string) || this.page;
+    const data = await this.findAll<T>(this.table, getPagination(page, limit));
+    return this.context.json(data, 200);
+  });
 
-  create = async () => {
-    try {
-      const body = await this.context.req.json();
-      const payload = this.schema?.parse(body) ?? body;
-      const newRecord = await this.createRecord<T>(this.table, payload);
-      return this.context.json(newRecord, 201);
-    } catch (error) {
-      if (error instanceof ZodError) {
-        return this.context.json(
-          {
-            message: "Data validation error",
-            errors: z.treeifyError(error),
-          },
-          400
-        );
-      }
-      console.error("Error creating record:", error);
-      return this.context.json({ error: "Internal server error" }, 500);
-    }
-  };
+  create = asyncHandler(async () => {
+    const body = await this.context.req.json();
+    const payload = (await this.schema?.parseAsync(body)) ?? body;
+    const newRecord = await this.createRecord<T>(this.table, payload);
+    return this.context.json(newRecord, 201);
+  });
 }
